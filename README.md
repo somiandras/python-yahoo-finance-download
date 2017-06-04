75d9b1d39474b558bc46159c58dbc1c350d73b6b
doktor_justice <somiandras@gmail.com>
2017-06-04 22:33:46 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Download data from Yahoo Finance
+
+Yahoo Finance changed how their stock data API works as of May, 2017. The data is still available on Yahoo Finance pages, but the new API uses authorization via a cookie and a 'crumb'. This module obtains these with an initial request and uses them for the subsequent requests.
+
+This was heavily influenced by __c0redumb's__ [yahoo_quote_download](https://github.com/c0redumb/yahoo_quote_download). In fact, I replicated the logic in a slightly different way just for fun and learning.
+
+For the Yahoo Finance API changes read __c0redumb's__ [description](https://github.com/c0redumb/yahoo_quote_download). 
+
+### get_series(ticker, data_type[, start_date])
+
+Returns the csv data for the specified data type (history, div or split) from the specified start date. Start date has to be a datetime object, it defaults to 20 years before today.
+
+### get_all_data(ticker[, start_date])
+
+Returns an iterator of all the three data types. Start date has to be a datetime object, it defaults to 20 years before today.
+
+Any comments and suggestions are welcome.
---
+++ yahoo_downloader.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+
+'''Data downloader for Yahoo Finance, inspired by https://github.com/c0redumb/yahoo_quote_download'''
+
+import requests
+import re
+from datetime import datetime
+import json
+
+DATA_TYPES = ['history', 'div', 'split']
+default_start_date = datetime.today().replace(year=datetime.today().year - 20)
+
+_cookie = None
+_crumb = None
+
+
+def _get_crumb_and_cookies():
+    '''Make an initial request to extract cookies and crumb to use in subsequent requests'''
+    global _crumb
+    global _cookie
+    url = 'https://finance.yahoo.com/quote/^GSPC'
+    r = requests.get(url)
+    if r.status_code == requests.codes.ok:
+        _cookie = r.cookies
+        search = re.search('\"CrumbStore\"\:\{\"crumb\"\:\"(.*)\"\}\,\"QuotePageStore\"', r.text)
+        if search is None:
+            raise Exception('No crumb in initial response')
+        else:
+            _crumb = search.group(1)
+    else:
+        r.raise_for_status()
+
+
+def get_series(ticker, data_type, start_date):
+    '''Returns the csv data for the specified data type (history, div or split) 
+    from the specified start date. Start date has to be a datetime object, 
+    it defaults to 20 years before today'''
+    params = {
+        'period1': int(start_date.timestamp()),
+        'period2': int(datetime.today().timestamp()),
+        'events': data_type,
+        'crumb': _crumb,
+        'interval': '1d'
+    }
+    url = 'https://query1.finance.yahoo.com/v7/finance/download/{}'.format(ticker)
+    r = requests.get(url, params=params, cookies=_cookie)
+    if r.status_code == requests.codes.ok:
+        return(r.text)
+    else:
+        raise Exception(r.text)
+
+
+def get_all_data(ticker, start_date=default_start_date):
+    '''Returns an iterator of all the three data types. Start date has to be 
+    a datetime object, it defaults to 20 years before today'''
+    if _cookie is None or _crumb is None:
+        _get_crumb_and_cookies()
+    data = None
+    for data_type in DATA_TYPES:
+        try:
+            data = get_series(ticker, data_type, start_date)
+        except Exception as e:
+            exc = json.loads(str(e))
+            # In case of occasional authorization error renew crumb and cookie and fetch data
+            # See here: https://github.com/c0redumb/yahoo_quote_download/issues/3
+            if 'finance' in exc:
+                _get_crumb_and_cookies()
+                data = get_series(ticker, data_type, start_date)
+            # Handle missing ticker error
+            elif 'chart' in exc and exc['chart']['error']['code'] == 'Not Found':
+                desc = exc['chart']['error']['description']
+                print('{} {}: {}'.format(ticker, data_type, desc))
+                data = None
+        else:
+            yield data
+
